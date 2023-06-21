003f93591cedc2032c75de9a41e3b8ab4e6b457d
Nallamekala Chinna Babu <131579656+chinnababun25@users.noreply.github.com>
2023-06-21 13:40:17 +0530

    Update calc.py

--- calc.py
+++ calc.py
@@ -16,6 +16,7 @@ def calculator():
     print("Welcome to the Calculator App!")
 
     while True:
+        # Display menu options
         print("\nSelect operation:")
         print("1. Add")
         print("2. Subtract")
@@ -23,28 +24,37 @@ def calculator():
         print("4. Divide")
         print("5. Exit")
 
+        # Prompt for user's choice
         choice = input("Enter your choice (1-5): ")
 
         if choice == '5':
+            # Exit the program if choice is 5
             print("Thank you for using the Calculator App!")
             break
 
+        # Prompt for input numbers
         num1 = float(input("Enter the first number: "))
         num2 = float(input("Enter the second number: "))
 
         if choice == '1':
+            # Call add() function and print the result
             result = add(num1, num2)
             print("Result: ", result)
         elif choice == '2':
+            # Call subtract() function and print the result
             result = subtract(num1, num2)
             print("Result: ", result)
         elif choice == '3':
+            # Call multiply() function and print the result
             result = multiply(num1, num2)
             print("Result: ", result)
         elif choice == '4':
+            # Call divide() function and print the result
             result = divide(num1, num2)
             print("Result: ", result)
         else:
+            # Handle invalid input
             print("Invalid input! Please try again.")
 
+# Call the calculator function to start the program
 calculator()
